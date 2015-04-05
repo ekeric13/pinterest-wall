029846a784b8018f23a30e8d4fa6ccb0c0104236
Eric Kennedy <ekeric13@gmail.com>
2015-04-05 02:36:28 -0700

    Create lightbox

--- client/app/components/WallView/WallViewComponents/Image.js
+++ client/app/components/WallView/WallViewComponents/Image.js
@@ -1,9 +1,25 @@
 var React = require("react");
+// var Lightbox = require("./Lightbox");
+var ReactBootstrap = require("react-bootstrap");
+var mui = require('material-ui');
+var Dialog = mui.Dialog;
+var Modal = ReactBootstrap.Modal;
+var ModalTrigger = ReactBootstrap.ModalTrigger;
+var LightboxModal = require("./LightboxModal");
+// var NewDialog = require("./NewDialog");
 
 var cx = React.addons.classSet;
 
 
 var Image = React.createClass({
+
+  displayName: 'Image',
+
+  getInitialState: function() {
+    return {
+      lightbox: false
+    };
+  },
 
   componentDidMount: function(){
     console.log("props " +this.props._id, this.props);
@@ -13,9 +29,24 @@ var Image = React.createClass({
     // if (this.props._id % 2 === 0) {
     //   $("#" + this.props._id).addClass("w2");
     // }
+    console.log("MOUNTED");
+  },
+
+  componentDidUpdate: function() {
+    console.log("MOUNTED4");
+    // if (this.state.lightbox) {
+    //   //activate materialize modal
+    //    $('#modal1').openModal();
+    //    // this.setState({lightbox : !this.state.lightbox});
+    // }
   },
 
   render: function(){
+    var standardActions = [
+      { text: 'Cancel' },
+      { text: 'Submit', onClick: this._onDialogSubmit }
+    ];
+
     var caption = this.props.imgCaption;
     var imageId = "image-"+this.props._id;
     // var w2 = this.props._id % 2 === 0 ? 'w2' : '';
@@ -26,27 +57,58 @@ var Image = React.createClass({
       h2Img = 'height2Img';
     }
     var classes = cx('image-container');
-    var imageClasses = cx('image', h2Img);
+    var imageClasses = cx('image', h2Img ,'modal-trigger');
     var captionClasses = cx('image-caption', h2Cap);
     console.log("key?")
     console.log(this.props.id);
+
+    // if (this.state.lightbox) {
+    // }
+    // if (lightboxModal === undefined) {
+    //   this.setState({lightbox : !this.state.lightbox});
+    // }
     return (
     <div id={this.props._id} className={classes}>
-      <div id={imageId} className={imageClasses} />
+      <LightboxModal {...this.props} />
+      <div id={imageId} className={imageClasses} onClick={this.lightboxOpen} />
       <div className={captionClasses}>
         <span> {caption} </span>
       </div>
       <div className="row favorite">
-              <span className="col offset-s2 s1 offset-m2 m1 offset-l2 l1 hearts-number">{this.props.hearts}</span>
-              <span className="col s2 m2 l2 ion-heart hearts-icon"/>
-
-              <span className="col offset-s2 s1 offset-m2 m1 offset-l2 l1 votes-number">{this.props.votes}</span>
-              <span className="col s2 m2 l2 ion-android-arrow-up votes-icon" />
-
+        <span className="col offset-s2 s1 offset-m2 m1 offset-l2 l1 hearts-number">{this.props.hearts}</span>
+        <span className="col s2 m2 l2 ion-heart hearts-icon" />
+        <span className="col offset-s2 s1 offset-m2 m1 offset-l2 l1 votes-number">{this.props.votes}</span>
+        <span className="col s2 m2 l2 ion-android-arrow-up votes-icon"/>
       </div>
+
+
+
     </div>
     );
+  },
+
+  lightboxOpen: function(e) {
+    e.preventDefault();
+    var selector = '#modal' +this.props.id;
+    $(selector).openModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+    });
+    // if (this.isMounted()) {
+    //   this.setState({lightbox : !this.state.lightbox});
+    // }
   }
+
+  // handleStandardDialogTouchTap: function() {
+  //   console.log("here");
+  //   // if (this.isMounted ()) {
+  //   //   this.setState ({lightbox : !this.state.lightbox});
+  //   // }
+  //     this.refs.standardDialog.show();
+  // }
+
 });
 
 module.exports = Image;
---
+++ client/app/components/WallView/WallViewComponents/LightboxModal.js
@@ -0,0 +1,71 @@
+var React = require('react');
+// var IdeaActions = require("../../../actions/IdeaActions");
+var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
+
+
+var LightboxModal = React.createClass({
+
+  mixins: [PureRenderMixin],
+
+  // delete: function(e) {
+  //   e.preventDefault();
+  //   //cancels call if id is empty or undefined
+  //   if(this.props.id === '' || undefined){
+  //     return;
+  //   }
+  //   if (this.isMounted()) {
+  //      // IdeaActions.delete({ id: this.props.id, owner: this.props.owner });
+  //   }
+  // },
+
+  componentDidMount: function() {
+    // $(".modal").css("width", "80%");
+    // var modalHeight = $(".lightbox").css("height");
+    // var modalHeightNum = +modalHeight.substring(0, modalHeight.length -2);
+    // var heightDifferential = Math.abs(window.innerHeight - modalHeightNum);
+    // var height = Math.min(window.innerHeight, modalHeightNum) - heightDifferential;
+
+    $("#image-" + this.props.id).css({"background-image": "url('"+this.props.imgSrc+"')"});
+    $("#avatar-" + this.props.id).css({"background-image": "url('"+this.props.avatar+"')"});
+  },
+  componentDidUpdate: function() {
+    // $(".modal").css("width", "80%");
+    // $("#modal" + this.props.id).css({"top": "5%"});
+  },
+
+  render: function() {
+    console.log("IMAGE ID", this.props.id);
+    var modalId = "modal" + this.props.id;
+    var imageId = "image-" + this.props.id;
+    var avatarId = "avatar-" + this.props.id;
+    console.log("TAGS", this.props.tags);
+    return (
+      <div>
+
+        <div id={modalId} className="modal lightbox">
+          <div className="modal-content">
+            <div className="user row">
+              <span id={avatarId} className="avatar col"/>
+              <span className="username col s1 m1 l1"><span className="float-text">{this.props.username}</span></span>
+            </div>
+            <div id={imageId} className='lightbox-image' />
+            <div className="lightbox-image-caption">
+              <span> {this.props.imgCaption} </span>
+            </div>
+            <div className="row lightbox-favorite">
+                    <span className="col offset-s1 s1 offset-m2 m1 offset-l2 l1 hearts-number">{this.props.hearts}</span>
+                    <span className="col offset-s1 s2 m2 l1 ion-heart hearts-icon" />
+
+                    <span className="col offset-s2 s1 offset-m2 m1 offset-l4 l1 votes-number">{this.props.votes}</span>
+                    <span className="col offset-s1 s2 m2 l1 ion-android-arrow-up votes-icon"/>
+
+            </div>
+            <div className="tags">{this.props.tags}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = LightboxModal;
