ff224a9b6193391c1b54c2eb5ac6090b207fb8d8
Eric Kennedy <ekeric13@gmail.com>
2015-04-06 23:09:01 -0700

    Add new lightbox

--- client/app/components/WallView/WallViewComponents/Image.js
+++ client/app/components/WallView/WallViewComponents/Image.js
@@ -37,14 +37,17 @@ var Image = React.createClass({
 
     var caption = this.props.imgCaption;
     var imageId = "image-"+this.props._id;
-    // var w2 = this.props._id % 2 === 0 ? 'w2' : '';
+    var imageLightboxHref = this.props.imgSrc;
+    var lightboxNum = this.props.id;
+    var w2 = this.props._id % 3 === 0 ? 'w2' : '';
+    var w3 = this.props._id % 4 === 0 ? 'w3' : '';
     var h2Cap;
     var h2Img;
     if (this.props._id % 2 === 0) {
       h2Cap = 'height2Cap';
       h2Img = 'height2Img';
     }
-    var classes = cx('image-container', 'layer', 'expand-width');
+    var classes = cx('image-container', 'layer', 'expand-width', w2, w3);
     var imageClasses = cx('image', h2Img ,'modal-trigger');
     var captionClasses = cx('image-caption', h2Cap);
 
@@ -53,54 +56,24 @@ var Image = React.createClass({
     var calculateRandomNum = Math.random() * ( max - min) + min;
     var dataDepth = calculateRandomNum.toString().substring(0,4);
 
-    // if (this.state.lightbox) {
-    // }
-    // if (lightboxModal === undefined) {
-    //   this.setState({lightbox : !this.state.lightbox});
-    // }
     return (
     <div id={this.props._id} className={classes} data-depth={dataDepth}>
-      <LightboxModal {...this.props} />
-      <div id={imageId} className={imageClasses} onClick={this.lightboxOpen} />
+      <a href={imageLightboxHref} data-lightbox="image-1" data-title={caption} ><div id={imageId} className={imageClasses} onClick={this.lightboxOpen} /></a>
       <div className={captionClasses}>
         <span> {caption} </span>
       </div>
       <div className="row favorite">
         <span className="col offset-s2 s1 offset-m2 m1 offset-l2 l1 hearts-number">{this.props.hearts}</span>
-        <span className="col s2 m2 l2 ion-heart hearts-icon" />
+        <span className="col s2 m2 l2 ion-heart hearts-icon pop" />
         <span className="col offset-s2 s1 offset-m2 m1 offset-l2 l1 votes-number">{this.props.votes}</span>
-        <span className="col s2 m2 l2 ion-android-arrow-up votes-icon"/>
+        <span className="col s2 m2 l2 ion-android-arrow-up votes-icon grow"/>
       </div>
       <div className="tags">{this.props.tags}</div>
 
 
     </div>
     );
-  },
-
-  lightboxOpen: function(e) {
-    // window.parallax.disable();
-    e.preventDefault();
-    var selector = '#modal' +this.props.id;
-    // Loop hole to get around parallax conflicting with modal
-    $(selector).appendTo("#main");
-
-
-    $(selector).openModal({
-      dismissible: true, // Modal can be dismissed by clicking outside of the modal
-      opacity: .5, // Opacity of modal background
-      in_duration: 300, // Transition in duration
-      out_duration: 200, // Transition out duration
-    });
   }
-
-  // handleStandardDialogTouchTap: function() {
-  //   console.log("here");
-  //   // if (this.isMounted ()) {
-  //   //   this.setState ({lightbox : !this.state.lightbox});
-  //   // }
-  //     this.refs.standardDialog.show();
-  // }
 
 });
 
